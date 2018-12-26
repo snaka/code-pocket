2ff3a2dcc7c1651a207ba8a29380f8b572a94935
snaka <snaka.gml@gmail.com>
2018-12-26 20:25:22 +0900

    不要なコードを削除

--- platform/aws/lambda@edge/auth-test/api.js
+++ platform/aws/lambda@edge/auth-test/api.js
@@ -21,28 +21,13 @@ function generateToken() {
 }
 
 function cookieValue() {
-  const date = new Date();
-  return `myToken=${generateToken()}; expires=${date.toGMTString()};`;
+  return `myToken=${generateToken()}; max-age=3600;`;
 }
 
 module.exports.hoge = (event, context, callback) => {
   console.log('event:', JSON.stringify(event));
-  const expiresAt = Date.now() + (60 * 60 * 1000); // 1 hour
-  const token = jwt.sign({
-    resources: [
-      {
-        "path": "/contents/1/004f2f0e-28fd-409f-b4c2-cadfce574566/fd401342/fd401342.png",
-        "expiresAt": expiresAt
-      },
-      {
-        "path": "/contents/123/2051363c-2348-4281-b079-984934b09da4/*",
-        "expiresAt": expiresAt
-      }
-    ]
-  }, pass, { expiresIn: '1h' } );
   callback(null, {
     "name": "hoge",
-    "token": token,
     "Cookie": cookieValue()
   });
 };
