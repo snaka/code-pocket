fc0638bd383ad5621615de21ac897d307f7f6f68
snaka <snaka.gml@gmail.com>
2020-01-06 23:35:40 +0900

    fetch public ip from STUN server

--- lang/javascript/wrtc/index.js
+++ lang/javascript/wrtc/index.js
@@ -1,1 +1,34 @@
-var WebRTC = require('wrtc')
+const { RTCPeerConnection } = require('wrtc')
+
+function getIPAddresses() {
+  return new Promise(resolve => {
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: ["stun:stun.l.google.com:19302"]
+        }
+      ]
+    })
+
+    const rslt = []
+
+    pc.addEventListener('icecandidate', e => {
+      console.log('on icecandidate', e)
+      pc.close()
+      resolve(rslt)
+    })
+    pc.addEventListener('negotiationneeded', async () => {
+      console.log('on negotiationneeded')
+      const offer = await pc.createOffer()
+      console.log('offer', offer)
+      await pc.setLocalDescription(offer)
+      console.log('negotiation finished')
+    })
+
+    pc.createDataChannel("")
+  })
+}
+
+getIPAddresses().then(result => {
+  console.log('result', result)
+})
