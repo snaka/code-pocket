31efcab3ced5610121ecc26054a65b66beda28c8
snaka <snaka.gml@gmail.com>
2020-02-07 21:55:03 +0900

    fix push script

--- platform/aws/ecs/rails/webapp/ecs/deploy/push.sh
+++ platform/aws/ecs/rails/webapp/ecs/deploy/push.sh
@@ -6,7 +6,7 @@ docker build -f Dockerfile -t snaka/webapp:ecs-${LC} .
 docker push snaka/webapp:ecs-${LC}
 
 # LAST_COMMIT をhashに置き換える
-sed "s/webapp:LAST_COMMIT/webapp:$LC/g" ecs/task-definitions/webapp.json > webapp.json.tmp
+sed "s/webapp:LAST_COMMIT/webapp:ecs-$LC/g" ecs/task-definitions/webapp.json > webapp.json.tmp
 
 # Task定義を登録しtmp削除
 aws ecs register-task-definition --cli-input-json file://webapp.json.tmp
