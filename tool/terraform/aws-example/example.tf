cfd299b0b166da0550824f9651e9c4848cfd351c
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2020-10-24 17:20:46 +0900

    update comment

--- tool/terraform/aws-example/example.tf
+++ tool/terraform/aws-example/example.tf
@@ -1,3 +1,5 @@
+# どのプロバイダを利用するかを Terraform に知らせる
+#   registry: https://registry.terraform.io/
 terraform {
   required_providers {
     aws = {
@@ -7,14 +9,16 @@ terraform {
   }
 }
 
-# リソースを提供する側
+# プロバイダの設定
 provider "aws" {
+  # AWS CLI の profile 設定のうちどれを使うかを指定
   profile = "default"
   region  = "us-west-2"
 }
 
 # リソースのタイプと名前
 # タイプはプレフィクスとしてプロバイダが含まれる
+#   ex. aws - provider, instance - type
 resource "aws_instance" "example" {
   # AMI ID はリージョン固有なので region を変更する際は変更する必要あり
   ami           = "ami-08d70e59c07c61a3a"
