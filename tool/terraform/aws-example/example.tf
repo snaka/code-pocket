973126b857674e2ee6b99d1a2c043877a83cefc7
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2020-10-24 17:51:32 +0900

    add output

--- tool/terraform/aws-example/example.tf
+++ tool/terraform/aws-example/example.tf
@@ -24,3 +24,12 @@ resource "aws_instance" "example" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
