3ea36fb08747b8d15249e1384abee92e0def85a8
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2021-03-18 07:45:24 +0900

    add slack bot source code

---
+++ framework/bolt/hello/app.js
@@ -0,0 +1,44 @@
+const { App, ExpressReceiver } = require('@slack/bolt');
+const serverlessExpress = require('@vendia/serverless-express');
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  processBeforeResponse: true
+});
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver
+});
+
+app.message('hello', async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>! (fallback)`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+module.exports.handler = serverlessExpress({
+  app: expressReceiver.app
+});
