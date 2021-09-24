38e8c8bac96973ad781b1dcd2d31690218244e0a
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2021-09-24 09:16:18 +0900

    Apollo: express server

--- framework/apollo/photo-share-api/index.js
+++ framework/apollo/photo-share-api/index.js
@@ -1,5 +1,7 @@
+const expressPlayground = require('graphql-playground-middleware-express').default
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
 const { GraphQLScalarType } = require('graphql')
-const { ApolloServer } = require('apollo-server')
 
 const typeDefs = `
   scalar DateTime
@@ -129,6 +131,7 @@ const resolvers = {
   })
 }
 
+/*
 const server = new ApolloServer({
   typeDefs,
   resolvers
@@ -137,3 +140,16 @@ const server = new ApolloServer({
 server
   .listen()
   .then(({url}) => console.log(`GraphQL Serfvice running on ${url}`))
+*/
+
+const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.applyMiddleware({ app })
+
+app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+app.get('/playground', expressPlayground({ endpoint: '/graphql'}))
+
+app.listen({ port: 4000 }, () =>
+  console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+)
