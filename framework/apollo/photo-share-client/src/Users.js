1b15e8b2adebfd7e24e2c03336f6fbc1b3776702
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2021-09-26 20:16:41 +0900

    framework/apollo/photo-share-client: Add mutation

--- framework/apollo/photo-share-client/src/Users.js
+++ framework/apollo/photo-share-client/src/Users.js
@@ -1,23 +1,39 @@
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import { ROOT_QUERY } from './App';
 
-console.log('Users.js');
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count:Int!) {
+    addFakeUsers(count:$count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`
 
 const Users = () =>
   <Query query={ROOT_QUERY}>
     {
-      ({ data, loading }) => {
-        console.log('loading:', loading);
-        return loading ?
-                <p>loading users...</p> :
-                <UserList count={data.totalUsers} users={data.allUsers} />
-      }
+      ({ data, loading, refetch }) => loading ?
+        <p>loading users...</p> :
+        <UserList count={data.totalUsers}
+          users={data.allUsers}
+          refetchUsers={refetch} />
     }
   </Query>;
 
-const UserList = ({ count, users }) =>
+const UserList = ({ count, users, refetchUsers }) =>
   <div>
     <p>{count} Users</p>
+    <button onClick={() => refetchUsers()}>Refetch Users</button>
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]}>
+      {addFakeUsers =>
+        <button onClick={addFakeUsers}>Add Fake Users</button>
+      }
+    </Mutation>
     <ul>
       {users.map(user =>
         <UserListItem key={user.githubLogin}
@@ -25,12 +41,12 @@ const UserList = ({ count, users }) =>
           avatar={user.avatar} />
       )}
     </ul>
-  </div>
+  </div>;
 
 const UserListItem = ({ name, avatar }) =>
   <li>
     <img src={avatar} width={48} height={48} alt="" />
     {name}
-  </li>
+  </li>;
 
 export default Users;
