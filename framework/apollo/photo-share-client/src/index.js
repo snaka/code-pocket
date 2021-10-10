6f2eb39a0f22bc77409550e34baf8340e6aac31f
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2021-10-10 19:32:29 +0900

    wip

--- framework/apollo/photo-share-client/src/index.js
+++ framework/apollo/photo-share-client/src/index.js
@@ -5,7 +5,17 @@ import App from './App';
 import { ApolloProvider  } from '@apollo/react-common';
 import ApolloClient from 'apollo-boost';
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  request: operation => {
+    operation.setContext(context => ({
+      headers: {
+        ...context.headers,
+        authorization: localStorage.getItem('token')
+      }
+    }))
+  }
+})
 
 render(
   <ApolloProvider client={client}>
